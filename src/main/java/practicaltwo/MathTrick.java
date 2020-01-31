21636f7dfbd8432a33e5bb19b1b99a6506bd443e
Douglas Luman <dluman@douglasjluman.com>
2020-01-31 09:53:38 -0500

    All my problems are language problems

--- src/main/java/practicaltwo/MathTrick.java
+++ src/main/java/practicaltwo/MathTrick.java
@@ -12,17 +12,17 @@ public class MathTrick {
    */
   public static void main(String[] args) {
     // Set up cards
-	int cardA = 8;
-	int cardB = 7;
-	int workingTotal;
-	int magicNumber = 10 - cardA;
-	// Do the trick
-	workingTotal = cardB * 2;
-	workingTotal += 2;
-	workingTotal *= 5;
-	workingTotal -= magicNumber;
-	// Print message
-	System.out.println("Your \"friendship number\" is: \t" + workingTotal);
-	System.out.println("Your original cards were: \t" + cardB + cardA);
+    int cardA = 8;
+    int cardB = 7;
+    int workingTotal;
+    int magicNumber = 10 - cardA;
+    // Do the trick
+    workingTotal = cardB * 2;
+    workingTotal += 2;
+    workingTotal *= 5;
+    workingTotal -= magicNumber;
+    // Print message
+    System.out.println("Your \"friendship number\" is: \t" + workingTotal);
+    System.out.println("Your original cards were: \t" + cardB + cardA);
   }
 }
